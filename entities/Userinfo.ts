6fddcc8266eb6a5c9b04d89b755d703fa9f70ce0
linhaorui <2909753663@qq.com>
2023-09-22 19:40:00 +0800

    feat: 连接数据库

---
+++ entities/Userinfo.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("userinfo", { schema: "nest-chat" })
+export class Userinfo {
+  @PrimaryGeneratedColumn({ type: "bigint", name: "id" })
+  id: string;
+
+  @Column("varchar", { name: "userName", comment: "姓名", length: 20 })
+  userName: string;
+
+  @Column("varchar", { name: "password", comment: "密码", length: 255 })
+  password: string;
+
+  @Column("varchar", {
+    name: "avatar",
+    nullable: true,
+    comment: "头像",
+    length: 255,
+  })
+  avatar: string | null;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { Userinfo } from 'entities/Userinfo';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'nest-chat',
+      logging: true,
+      entities: [Userinfo],
+      // 如果配置了 synchronize，还会生成建表 sql 语句来创建表。
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
