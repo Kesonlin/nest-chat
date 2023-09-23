3368d4d96e226c36cbae2f0d8902e093faf0ba78
linhaorui <2909753663@qq.com>
2023-09-23 21:51:32 +0800

    feat: websocket增加房间功能

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -3,6 +3,7 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { SocketService } from './socket.service';
 import { Socket } from 'dgram';
@@ -11,16 +12,19 @@ import { CreateMessageDto } from './dto';
 @WebSocketGateway({
   cors: {
     origin: '*',
-    // SO_bro
   },
 })
 export class SocketGateway {
   constructor(private readonly socketService: SocketService) {}
 
+  @WebSocketServer() server: any;
+
   @SubscribeMessage('connection')
-  create(@MessageBody() body: any, @ConnectedSocket() client: Socket) {
-    console.log(body);
-    // client.emit('message', body);
+  create(@MessageBody() body: any, @ConnectedSocket() client: any) {
+    client.emit('join', () => {
+      client.join('lin');
+    });
+    // return { event: 'join', data: '服务端推送到客户端' };
   }
 
   @SubscribeMessage('sendMessage')
@@ -31,7 +35,8 @@ export class SocketGateway {
     console.log('sendMessage', body);
     this.socketService.create(body);
     // 广播的话自己收不到
-    client.broadcast.emit('showMessage', `boradcast ${body}`);
+    // client.broadcast.emit('showMessage', `boradcast ${body}`);
+    this.server.to('lin').emit('showMessage', body.message);
     client.emit('showMessage', body);
   }
 }
