8eda974f04f2c9163bd65d548a10cbdf340b24f9
linhaorui <2909753663@qq.com>
2023-10-06 22:50:28 +0800

    feat: 支持静态文件服务

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { Userinfo } from 'entities/Userinfo';
 import { SocketModule } from './socket/socket.module';
 import { Message } from 'entities/Message';
+import { join } from 'path';
 
 console.log(process.env.PASS, 'process.env.PASS');
 
@@ -23,7 +25,11 @@ console.log(process.env.PASS, 'process.env.PASS');
       logging: true,
       entities: [Userinfo, Message],
       // 如果配置了 synchronize，还会生成建表 sql 语句来创建表。112233 部署成功了
-      synchronize: true,
+      synchronize: false,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload'),
+      // serveStaticOptions: { index: false },
     }),
     UserModule,
     SocketModule,
